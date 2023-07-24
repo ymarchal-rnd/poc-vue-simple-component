098daa99f34a5791e5461f47e72e965e4ae62a3f
Yoan Marchal <ymarchal@rnd.fr>
2023-07-24 12:00:11 +0200

    remove all exept dist

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { ref } from "vue";
-const sliderValue = ref(50);
-</script>
-<template>
-  <input v-model="sliderValue" type="range" min="0" max="100" class="slider" />
-  <br />
-  <input v-model="sliderValue" type="number" />
-</template>
